ff4c951b1c88f06ed252febc6d3f2cd63984a554
mirabelkoso <mirabelkoso@gmail.com>
2015-01-20 14:47:17 +0100

    modified the route page

--- app/js/custom/app.js
+++ app/js/custom/app.js
@@ -2,31 +2,31 @@ var FeedAChildApp = angular.module('FeedAChildApp', ['ngRoute']);
 FeedAChildApp.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
   $locationProvider.html5Mode(true);
   $routeProvider.
-    when('/feedAChild-MEAN', {
+    when('/feedAChild-MEAN/app', {
       templateUrl: 'index.html',
       controller: 'mainCtrl'
     }).
 
-    when('/feedAChild-MEAN', {
+    when('/feedAChild-MEAN/app', {
       templateUrl: 'home.html',
       controller: 'mainCtrl'
     }).
 
-    when('/feedAChild-MEAN/form', {
+    when('/feedAChild-MEAN/app/form', {
       templateUrl: 'form.html',
       controller: 'formCtrl'
     }).
 
-    when('/feedAChild-MEAN/contact', {
+    when('/feedAChild-MEAN/app/contact', {
       templateUrl: 'contact.html',
       controller: 'contactCtrl'
     }).
 
-    when('/feedAChild-MEAN/children', {
+    when('/feedAChild-MEAN/app/children', {
       templateUrl: 'children.html',
       controller: 'childrenCtrl'
     }).
-    when('/feedAChild-MEAN/:id/delete', {
+    when('/feedAChild-MEAN/app/:id/delete', {
       templateUrl: 'contact.html',
       controller: 'deleteCtrl'
     }).
